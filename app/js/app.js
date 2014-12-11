a6e492c64e30be4a94b3d5e5a57d53d2b0175c54
LAU Thierry <thierlau@gmail.com>
2014-12-11 21:31:13 +0100

    update package

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,8 @@ angular.module('liste-noel', ['firebase']);
 
 
 angular.module('liste-noel').run(function () {
+	"use strict";
+
 	$('.parallax').parallax();
 	$('.materialboxed').materialbox();
 });
