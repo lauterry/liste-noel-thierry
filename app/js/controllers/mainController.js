4b61749c385600601ceeb45b45f4b5849c7adee0
LAU Thierry <thierlau@gmail.com>
2014-12-15 00:47:22 +0100

    fix rules and json

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -6,10 +6,16 @@ angular.module('liste-noel').controller('mainController', function ($scope, gift
 	this.confirmParticipation = function () {
 
 		var gift = giftService.getByRecordKey(this.gift.$id);
-		gift.remaining = this.gift.price  - this.participation;
-		giftService.save(gift).then(function () {
+		gift.remaining = gift.remaining  - this.participation;
+
+		if (gift.remaining <= 0) {
+			gift.bought = true;
+		}
+
+		giftService.save(gift).then(angular.bind(this, function () {
+			this.participation = undefined;
 			$log.log('Gift saved !');
-		});
+		}));
 	};
 
 	this.confirmBuy = function () {
