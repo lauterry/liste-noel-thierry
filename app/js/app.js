31343a3c73124dd6c119a8450bc80ab9a440d5cf
LAU Thierry <thierlau@gmail.com>
2015-01-11 18:22:50 +0100

    use ng-include to include modal

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,23 @@
-angular.module('liste-noel', ['firebase']);
+angular.module('liste-noel', ['firebase', 'ngRoute']);
+
+angular.module('liste-noel').config(function ($routeProvider) {
+	"use strict";
+
+	$routeProvider
+		.when('/list', {
+			templateUrl : '/partials/list.html',
+			controller : "mainController",
+			controllerAs : "main"
+		})
+		.when('/admin', {
+			templateUrl : '/partials/admin.html',
+			controller : "adminController",
+			controllerAs : "adminCtrl"
+		})
+		.otherwise({
+			redirectTo: '/list'
+		})
+});
 
 
 angular.module('liste-noel').run(function () {
